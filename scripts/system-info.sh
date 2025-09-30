bf7d30e8a2501b8bed0322a32cd7e971cc4d840b
FelipeRmz29 <lfelipe.ramirez@edu.uag.mx>
2025-09-29 18:03:43 -0600

    Update system-info.sh

--- scripts/system-info.sh
+++ scripts/system-info.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+# Script para recopilar informacion basica del sistema y GRUB
+
+echo "--- Informacion del Sistema y GRUB ---"
+echo ""
+
+echo "1. Informacion de la Distribucion:"
+lsb_release -a
+echo "-----------------------------------"
+
+echo "2. Informacion del Kernel y Arquitectura:"
+uname -a
+echo "-----------------------------------"
+
+echo "3. Dispositivos de Bloque (Discos/Particiones):"
+lsblk
+echo "-----------------------------------"
+
+echo "4. Version de GRUB Instalada:"
+sudo grub-install --version
+echo "-----------------------------------"
 
